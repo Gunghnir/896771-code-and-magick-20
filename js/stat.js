5028fe927e5d6babae54025ec49f520c11f9c190
Seriapov <Vladimir.Seriapov@thomsonreuters.com>
2020-06-14 16:12:22 +0200

    fixes applied

--- js/stat.js
+++ js/stat.js
@@ -8,7 +8,9 @@ var GAP = 10;
 var BAR_GAP = 90;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
-
+var TIME_X = 50;
+var TIME_Y = 65;
+var BAR_START_Y = 80;
 
 // Создаем Облако
 var renderCloud = function (ctx, x, y, color) {
@@ -38,26 +40,26 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillText('Ура вы победили!', 2 * CLOUD_X, 3 * GAP);
   ctx.fillText('Список результатов:', 2 * CLOUD_X, 5 * GAP);
 
-  var MaxTime = getMaxElement(times);
+  var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
     ctx.font = '16px PT Mono';
     ctx.fillText(players[i], CLOUD_X / 2 + (i + 1) * BAR_GAP, CLOUD_Y + 1.5 * BAR_HEIGHT + 2 * GAP);
 
-    var stepSize = BAR_HEIGHT * times[i] / MaxTime;
+    var stepSize = BAR_HEIGHT * times[i] / maxTime;
     ctx.fillStyle = fillBarColor(players[i]);
-    ctx.fillRect(CLOUD_X / 2 + BAR_GAP * (i + 1), CLOUD_Y + 8 * GAP + BAR_HEIGHT - stepSize, BAR_WIDTH, stepSize);
-    ctx.fillText(times[i].toFixed(0), 50 + (i + 1) * BAR_GAP, CLOUD_Y + 65);
+    ctx.fillRect(CLOUD_X / 2 + BAR_GAP * (i + 1), CLOUD_Y + BAR_START_Y + BAR_HEIGHT - stepSize, BAR_WIDTH, stepSize);
+    ctx.fillText(times[i].toFixed(0), TIME_X + (i + 1) * BAR_GAP, BAR_START_Y + BAR_HEIGHT - stepSize);
   }
 
   // Вычисляем цвет бара в зависимости от имени игрока
   function fillBarColor(namePlayer) {
-    var randomOpacity = Math.random().toFixed(2);
+    var randomTransparency = Math.random().toFixed(2);
     if (namePlayer === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + randomOpacity + ')';
+      ctx.fillStyle = 'rgb(0, 26, 255, ' + randomTransparency + ')';
     }
   }
 };
